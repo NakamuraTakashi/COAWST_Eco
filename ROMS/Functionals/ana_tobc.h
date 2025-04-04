40097bb84fde33d82490d2e695d1cb8a0d193560
Takashi NAKAMURA <nakamura.t.av@m.titech.ac.jp>
2025-04-04 20:56:09 +0900

    Small bugfix: ana_tobc.h

--- ROMS/Functionals/ana_tobc.h
+++ ROMS/Functionals/ana_tobc.h
@@ -1095,7 +1095,13 @@
 #else
       IF (ANY(LBC(ieast,isTvar(:),ng)%acquire).and.                     &
      &    DOMAIN(ng)%Eastern_Edge(tile)) THEN
-        DO itrc=1,NT(ng)
+        DO k=1,N(ng)
+          DO j=JstrT,JendT
+            BOUNDARY(ng)%t_east(j,k,itemp)=T0(ng)
+            BOUNDARY(ng)%t_east(j,k,isalt)=S0(ng)
+          END DO
+        END DO
+        DO itrc=3,NT(ng)
           DO k=1,N(ng)
             DO j=JstrT,JendT
               BOUNDARY(ng)%t_east(j,k,itrc)=0.0_r8
@@ -1106,7 +1112,13 @@
 
       IF (ANY(LBC(iwest,isTvar(:),ng)%acquire).and.                     &
      &    DOMAIN(ng)%Western_Edge(tile)) THEN
-        DO itrc=1,NT(ng)
+        DO k=1,N(ng)
+          DO j=JstrT,JendT
+            BOUNDARY(ng)%t_west(j,k,itemp)=T0(ng)
+            BOUNDARY(ng)%t_west(j,k,isalt)=S0(ng)
+          END DO
+        END DO
+        DO itrc=3,NT(ng)
           DO k=1,N(ng)
             DO j=JstrT,JendT
               BOUNDARY(ng)%t_west(j,k,itrc)=0.0_r8
@@ -1117,7 +1129,13 @@
 
       IF (ANY(LBC(isouth,isTvar(:),ng)%acquire).and.                    &
      &    DOMAIN(ng)%Southern_Edge(tile)) THEN
-        DO itrc=1,NT(ng)
+        DO k=1,N(ng)
+          DO i=IstrT,IendT
+            BOUNDARY(ng)%t_south(i,k,itemp)=T0(ng)
+            BOUNDARY(ng)%t_south(i,k,isalt)=S0(ng)
+          END DO
+        END DO
+        DO itrc=3,NT(ng)
           DO k=1,N(ng)
             DO i=IstrT,IendT
               BOUNDARY(ng)%t_south(i,k,itrc)=0.0_r8
@@ -1128,7 +1146,13 @@
 
       IF (ANY(LBC(inorth,isTvar(:),ng)%acquire).and.                    &
      &    DOMAIN(ng)%Northern_Edge(tile)) THEN
-        DO itrc=1,NT(ng)
+        DO k=1,N(ng)
+          DO i=IstrT,IendT
+            BOUNDARY(ng)%t_north(i,k,itemp)=T0(ng)
+            BOUNDARY(ng)%t_north(i,k,isalt)=S0(ng)
+          END DO
+        END DO
+        DO itrc=3,NT(ng)
           DO k=1,N(ng)
             DO i=IstrT,IendT
               BOUNDARY(ng)%t_north(i,k,itrc)=0.0_r8
